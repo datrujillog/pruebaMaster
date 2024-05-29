b27441c38d895385adf998420aace3f2b331c626
Diego Trujillo <diegotrujillo0210@gmail.com>
2024-05-29 09:44:10 -0500

    Feat: Added async getPopularRepositories(owner) method to get a user's popular repositories on GitHub

--- 01_apiGitHub.js/service/githubService.js
+++ 01_apiGitHub.js/service/githubService.js
@@ -13,6 +13,30 @@ class GithubService {
 
     async getPopularRepositories(owner) {
 
+        try {
+
+            const response = await axios.get(`https://api.github.com/users/${owner}/repos`, {
+                // params: {
+                //     sort: 'stars',
+                // per_page: 10
+                // },
+            });
+            const popularRepos = response.data
+                .map(repo => ({
+                    name: repo.name,
+                    stars: repo.stargazers_count,
+                    url: repo.html_url,
+                }))
+                .sort((a, b) => b.stars - a.stars)
+                .slice(0, 10);
+            console.log(popularRepos);
+            return {
+                popularRepos: popularRepos,
+            }
+
+        } catch (error) {
+            console.error('Error al obtener los repositorios:', error.message);
+        }
     }
 
 }
